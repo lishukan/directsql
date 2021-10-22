b530e7798a00bb30e923b5d0810702c5035de18d
lishukan <lishukan@dxy.cn>
2021-10-22 14:13:20 +0800

    setup.py 更新

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
   
 setup(  
     name = 'directsql',  
-    version='0.3.0',
+    version='0.3.1',
     # keywords = ('chinesename',),  
     description="""一个简单的使用python操作mysql的工具，提供了一些类似sql语法的方法，最终拼接成sql。可以很好地处理一些常见场景，不依赖orm 的同时避免手写大量sql。""",  
     license = 'MIT License',  
