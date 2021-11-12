5bfd911c31ccb9c7ae0f9891c402ac5f5c7d32ad
lishukan <lishukan@dxy.cn>
2021-11-12 16:21:40 +0800

    合并成一个文件

--- directsql/__init__.py
+++ directsql/__init__.py
@@ -1,7 +1,6 @@
 
-__version__ = "0.3.1"
+__version__ = "0.4.0"
 
 
-from .sqlgenerator import SqlGenerator, MysqlSqler
-from .connector import MysqlPool, MysqlConnection
+from .connector import MysqlPool, MysqlConnection,SqlGenerator,MysqlSqler
 __all__ = ["SqlGenerator", "MysqlConnection", "MysqlPool", "MysqlSqler"]
